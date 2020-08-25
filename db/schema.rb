ed6ada4c4145925f7684761676996df14317cc21
chipojaya1 <jaya@diveintocode.jp>
2020-08-25 03:51:08 +0000

    appending top page

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_013629) do
+ActiveRecord::Schema.define(version: 2020_08_25_025319) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-create_table "blogs", force: :cascade do |t|
+  create_table "blogs", force: :cascade do |t|
     t.text "content"
-    t.datetime "dateposted", null: false
-    t.datetime "updated", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end

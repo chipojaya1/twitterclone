e274d9b2f22530a4b9ad9d266bd1688fed9221fc
chipojaya1 <jaya@diveintocode.jp>
2020-08-31 03:34:48 +0000

    confirmation and modification

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,11 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
-    @blogs = Blog.all
-    @blog = Blog.new(blog_params)
-  end
-
-  def show
+    @blog = Blog.all
   end
 
   def new
@@ -15,18 +11,24 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "blog successfully created"
-    else
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "blog successfully created"
+      else
+        render :new
+      end
     end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "blog succefully updated!"
     else
@@ -45,9 +47,11 @@ class BlogsController < ApplicationController
   end
 
   private
+
   def blog_params
-    params.require(:blog).permit(:blog)
+    params.require(:blog).permit(:content)
   end
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
